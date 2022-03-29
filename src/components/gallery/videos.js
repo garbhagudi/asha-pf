511c1190193b330a82c9ca1e664015299e45b5d2
garbhagudi <garbhagudi.11@gmail.com>
2022-03-29 13:42:01 +0530

    minor improvements and updates

--- src/components/gallery/videos.js
+++ src/components/gallery/videos.js
@@ -2,68 +2,56 @@ import React from "react";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "./ytlite.css";
 
+const Data = [
+  {
+    id: 1,
+    title:
+      "FertiliTEA -1: All you want to know about PCOS and it's impact on fertility.",
+    videoId: "tc4NcHjdxqg",
+  },
+  {
+    id: 2,
+    title: "FertiliTEA -2: Male Infertility An Overview Of Causes",
+    videoId: "FYo4I4dxym0",
+  },
+  {
+    id: 3,
+    title: "FertiliTEA -3: Painful periods(Dysmenorrhoea) and Infertility.",
+    videoId: "TLoFZqBVw_E",
+  },
+  {
+    id: 4,
+    title: "FertiliTEA -4: Causes and Solutions for Multiple IUI Failure",
+    videoId: "2ik83PqCL6I",
+  },
+  {
+    id: 5,
+    title: "FertiliTEA -5: Causes and Solutions for Multiple IVF Failure",
+    videoId: "hZvphnF-tBk",
+  },
+  {
+    id: 6,
+    title: "FertiliTEA -6: Male Infertility and its Treatments",
+    videoId: "VxWns8mqKzc",
+  },
+];
+
 const Videos = () => {
   return (
     <div className="container mx-auto px-4">
-      <section className="py-8 px-4">
+      <section className="py-8 px-4 sm:px-2">
         <div className="flex flex-wrap -mx-4">
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="tc4NcHjdxqg" poster="maxresdefault" />
+          {Data.map((item) => (
+            <div
+              className="md:w-1/2 px-0 sm:p-3 mb-8 md:mb-0 text-center"
+              key={item.id}
+            >
+              <div className="rounded-2xl overflow-hidden">
+                <LiteYouTubeEmbed id={item.videoId} poster="maxresdefault" />
+              </div>
+              <span className="text-xl font-qs">{item.title}</span>
             </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -1: All you want to know about PCOS and it's impact on
-              fertility.
-            </span>
-          </div>
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="FYo4I4dxym0" poster="maxresdefault" />
-            </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -2: Male Infertility An Overview Of Causes
-            </span>
-          </div>
-        </div>
-      </section>
-      <section className="py-8 px-4">
-        <div className="flex flex-wrap -mx-4">
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="TLoFZqBVw_E" poster="maxresdefault" />
-            </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -3: Painful periods(Dysmenorrhoea) and Infertility.
-            </span>
-          </div>
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="2ik83PqCL6I" poster="maxresdefault" />
-            </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -4: Causes and Solutions for Multiple IUI Failure
-            </span>
-          </div>
-        </div>
-      </section>
-      <section className="py-8 px-4">
-        <div className="flex flex-wrap -mx-4">
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="hZvphnF-tBk" poster="maxresdefault" />
-            </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -5: Causes and Solutions for Multiple IVF Failure
-            </span>
-          </div>
-          <div className="md:w-1/2 px-4 mb-8 md:mb-0 text-center">
-            <div>
-              <LiteYouTubeEmbed id="VxWns8mqKzc" poster="maxresdefault" />
-            </div>
-            <span className="text-xl font-qs">
-              FertiliTEA -6: Male Infertility and its Treatments
-            </span>
-          </div>
+          ))}
         </div>
       </section>
     </div>
--- src/components/home/breakthrough.js
+++ src/components/home/breakthrough.js
@@ -80,7 +80,7 @@ const Breakthrough = () => {
                       >
                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                       </svg>
-                      <span class="text-gray-600 ml-3">92 Reviews</span>
+                      <span class="text-gray-600 ml-3">162 Reviews</span>
                     </span>
                   </div>
                   <p class="leading-relaxed">
@@ -138,6 +138,8 @@ const Breakthrough = () => {
                       <a
                         href="https://www.amazon.in/BREAKTHROUGH-11-Trailblazers-One-Movement/dp/9390260272"
                         alt="buy-now"
+                        target={"_blank"}
+                        rel="noreferrer"
                       >
                         Buy Now!
                       </a>
